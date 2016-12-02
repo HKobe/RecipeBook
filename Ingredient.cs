90a9d37e455aed65476ca2b13a3a349bd19bb355
HKobe <HGKobeissi1@gmail.com>
2016-12-02 15:46:23 -0500

    Add files via upload

---
+++ Ingredient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Ingredient
+/// </summary>
+public class Ingredient
+{
+	public Ingredient()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    private string recipeName;
+    public string RecipeName
+    {
+        get
+        {
+            return recipeName;
+        }
+        set
+        {
+            recipeName = value;
+        }
+    }
+
+    private string recipeOwner;
+    public string RecipeOwner
+    {
+        get { return recipeOwner; }
+        set { recipeOwner = value; }
+    }
+
+    private int cookingTime;
+    public int CookingTime
+    {
+        get { return cookingTime; }
+        set { cookingTime = value; }
+    } 
+}
---
+++ IngredientRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for IngredientRepository
+/// </summary>
+public class IngredientRepository
+{
+	public IngredientRepository()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public List<Ingredient> GetIngredients()
+    {
+        HttpApplication webApp = HttpContext.Current.ApplicationInstance;
+        return (List<Ingredient>)webApp.Application["ingredients"];
+    }
+}
